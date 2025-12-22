56127e9cc2707292e328435b818cc3b74cc7103f
brunocarvente <new.shawn@hotmail.com>
2025-12-22 03:31:14 -0300

    Update EnemyService.lua

--- src/ServerScriptService/Modules/EnemyService.lua
+++ src/ServerScriptService/Modules/EnemyService.lua
@@ -1,7 +1,21 @@
+local replicatedStorage = game:GetService("ReplicatedStorage")
+
 local EnemyService = {}
 
+
+function EnemyService:SpawnEnemy()
+    local killer = replicatedStorage:FindFirstChild("Enemy"):Clone()
+    killer.Parent = workspace
+    task.spawn(function()
+        while killer.Parent do
+            killer.Humanoid:MoveTo(workspace.PlayerSpawn.Position + Vector3.new(math.random(-10,10),0,math.random(-10,10)))
+            task.wait(1)
+        end
+    end)
+end
+
 function EnemyService:Init()
-    print("OI BUGOU")
+    self:SpawnEnemy()
 end
 
 

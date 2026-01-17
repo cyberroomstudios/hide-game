69a08c9b66b1380e22e64207eb6947fd034d6ac4
Alysson Chicó <cyberroomstudios@gmail.com>
2026-01-17 01:52:05 -0300

    Adding Be The Killer Developer Product

--- src/StarterPlayer/StarterPlayerScripts/ClientModules/HudController.lua
+++ src/StarterPlayer/StarterPlayerScripts/ClientModules/HudController.lua
@@ -4,6 +4,7 @@ local Players = game:GetService("Players")
 local UIReferences = require(Players.LocalPlayer.PlayerScripts.Util.UIReferences)
 local DeveloperProductController = require(Players.LocalPlayer.PlayerScripts.ClientModules.DeveloperProductController)
 
+local beTheKiller
 
 function HudController:Init()
 	HudController:CreateReferences()
@@ -11,17 +12,16 @@ function HudController:Init()
 end
 
 function HudController:CreateReferences()
+	beTheKiller = UIReferences:GetReference("BE_THE_KILLER_BUTTON")
 end
 
 function HudController:InitButtonListerns()
-
+	beTheKiller.MouseButton1Click:Connect(function()
+		DeveloperProductController:OpenPaymentRequestScreen("BE_THE_KILLER")
+	end)
 end
 
-function HudController:HideDefaultHud()
-	
-end
+function HudController:HideDefaultHud() end
 
-function HudController:ShowDefaultHud()
-
-end
+function HudController:ShowDefaultHud() end
 return HudController
